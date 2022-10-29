8a1e2660f2806df3aedac5972992e26a4f622ec1
Obito Uchiha <115386722+Obito1984@users.noreply.github.com>
2022-10-29 15:50:06 +0530

    Update afk_sql.py

--- Grafiabot/Modules/sql/afk_sql.py
+++ Grafiabot/Modules/sql/afk_sql.py
@@ -1,6 +1,6 @@
 import threading
 
-from Bestie_Robot.modules.sql import BASE, SESSION
+from GrayfiaBot .modules.sql import BASE, SESSION
 from sqlalchemy import Boolean, Column, Integer, UnicodeText
 from sqlalchemy.sql.sqltypes import BigInteger
 
