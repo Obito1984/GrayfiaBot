6b7a67eaa4e54ef4e1782a5aec6b5be0096a90b0
Obito Uchiha <115386722+Obito1984@users.noreply.github.com>
2022-10-29 15:48:41 +0530

    Update __init__.py

--- Grafiabot/Modules/sql/__init__.py
+++ Grafiabot/Modules/sql/__init__.py
@@ -1,4 +1,4 @@
-from Bestie_Robot import DB_URL
+from GrayfiaBot import DB_URL
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
